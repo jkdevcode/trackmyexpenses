6c02d9ff0c0ef3803e75d129ee326599a92770b0
josedvargas31 <dajozavargas@gmail.com>
2025-08-18 20:29:08 -0500

    feat(backend): Add controllers and  routes lists users and delete user

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { authGuard } from "../middlewares/auth.middleware.js";
-import { me, updateProfile } from "../controllers/user.controller.js";
+import { me, updateProfile, getAllUsers, deleteUser } from "../controllers/user.controller.js";
 import { validateUpdateProfile } from "../validations/user.validation.js";
 
 const router = Router();
 
 router.get("/me", authGuard, me);
 router.put("/me", authGuard, validateUpdateProfile, updateProfile);
+router.get("/", authGuard, getAllUsers);
+router.delete("/:id", authGuard, deleteUser);
 
 export default router;
