12612df75f960397e0f14d68941bc73a4e3e6814
josedvargas31 <dajozavargas@gmail.com>
2025-09-10 21:51:06 -0500

    feat(frontend): Add frontend

--- backend/src/middlewares/errorHandler.ts
+++
@@ -1,9 +0,0 @@
-import type { NextFunction, Request, Response } from "express";
-
-function errorHandler(err: any, _req: Request, res: Response, _next: NextFunction) {
-  console.error("❌", err);
-  const status = err.status || 500;
-  res.status(status).json({ error: err.message || "Internal Server Error" });
-}
-
-module.exports = { errorHandler };
